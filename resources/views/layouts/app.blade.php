e5920ff25d4789959a802dbab0086215927a9f8d
vittorioaquilino <vittorioaquilino17@gmail.com>
2022-06-26 17:35:55 +0200

    add app.blade

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    
+    @include('partial.header')
+    <main>
+        @yield('main_content')
+    </main>
+    @include('partial.footer')
+</body>
+</html>
